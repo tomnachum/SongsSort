634af4cae3f00fbfe3ab77e829a6caa6947ee600
tomnachum <tom.nachum@gmail.com>
2023-11-11 18:24:48 +0200

    fixed logs

--- business_logic/albums_logic/albums_logic.py
+++ business_logic/albums_logic/albums_logic.py
@@ -21,11 +21,18 @@ class AlbumsLogic:
         self._logger.test("All tracks before filtering:\n" + tracks_to_json(all_tracks), total_tracks=len(all_tracks))
 
         filtered_tracks = filter(lambda t: self.filter_tracks(t, track, artist), all_tracks)
+
+        if not filtered_tracks:
+            self._logger.error('After filtering albums, 0 albums remained.', artist=artist, track=track)
+            raise ValueError
+
         sorted_tracks = sorted(filtered_tracks, key=self.tracks_comparator, reverse=True)
 
         if not sorted_tracks:
             self._logger.error('After sorting albums, 0 albums remained.', artist=artist, track=track)
             raise ValueError
+
+        self._logger.test("All tracks after filtering:\n" + tracks_to_json(sorted_tracks), total_tracks=len(all_tracks))
 
         track_entity = sorted_tracks[0]
         album = track_entity.album
@@ -34,8 +41,11 @@ class AlbumsLogic:
 
         if '(' in album.name:
             album_name_no_parentheses = album.name.split(' (')[0]
-            self._logger.info('Changed album name', original=album.name, new=album_name_no_parentheses, artist=artist,
-                              track=track)
+            if all(word not in album.name for word in
+                   ['Remastered', 'Delux', 'Expanded', 'New', 'Remaster', 'Edition', 'Mix']):
+                self._logger.info('Changed album name', original=album.name, new=album_name_no_parentheses,
+                                  artist=artist,
+                                  track=track)
             album.name = album_name_no_parentheses
 
         return album
@@ -70,27 +80,34 @@ class AlbumsLogic:
         return 10000 * compare_by_album_type + compare_by_release_year + compare_by_album_popularity
 
     # if this function returns False, the track will be removed
-    def filter_tracks(self, track: TrackEntity, expected_track_name: str = '',
-                      expected_track_artist: str = '') -> bool:
+    def filter_tracks(self, track: TrackEntity, expected_track_name: str = '', expected_track_artist: str = '') -> bool:
         try:
             if not track.album or not track.album.images:
                 self._logger.test("album not exists or picture not exist")
                 return False
-            if any((word.lower() in ['live', 'concert']) for word in
-                   track.album.name.replace('(', '').replace(')', '').split()) and \
+            if any((word.lower() in ['live', 'concert', 'karaoke']) for word in
+                   track.album.name.replace('(', '').replace(')', '').replace('[', '').replace(']', '').replace(',',
+                                                                                                                '').split()) and \
                     all(('live' not in word.lower()) for word in expected_track_name.split()):
-                self._logger.test("live or concert in album name", track_name_in_spotify=track.name)
+                self._logger.test("live or concert in album name", album_name=track.album.name,
+                                  track_name_in_spotify=track.name, expected_track_name=expected_track_name)
                 return False
             actual_artists = list(set([a.name for a in track.album.artists]) - {'Various Artists'})
             if actual_artists and len(actual_artists) > 1 and all(
                     [(unidecode(expected_track_artist) not in unidecode(artist_name)
                       and unidecode(artist_name) not in unidecode(expected_track_artist))
                      for artist_name in actual_artists]):
-                self._logger.test("artist not in album artists", album=json.dumps(track.album.model_dump()))
+                self._logger.test("artist not in album artists", album_name=track.album.name,
+                                  album_artists=actual_artists, expected_track_artist=f'{expected_track_artist}.')
                 return False
-            if actual_artists and len(actual_artists) == 1 and actual_artists[0] != expected_track_artist:
-                self._logger.test("artist is not equal to album artist", album=json.dumps(track.album.model_dump()))
-                return False
+            if actual_artists and len(actual_artists) == 1 and unidecode(actual_artists[0]) != unidecode(
+                    expected_track_artist):
+                self._logger.test("artist is not equal to album artist", album_name=track.album.name,
+                                  album_artists=actual_artists, expected_track_artist=f'{expected_track_artist}.')
+                if unidecode(expected_track_artist) not in unidecode(actual_artists[0]):
+                    self._logger.test("artist is not even included in album artist", album_name=track.album.name,
+                                      album_artists=actual_artists, expected_track_artist=f'{expected_track_artist}.')
+                    return False
             return True
         except Exception as e:
             self._logger.test('filter exception', error=e)
